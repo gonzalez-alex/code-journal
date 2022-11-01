196693b96ba7c5ac11bf62bdef80fdbca6675556
Alex Gonzalez <itsgonzalezalex@gmail.com>
2022-11-01 11:28:44 +0000

    Listen for 'submit' events on the journal entry form and: Put the form's input values into a new object, add nextEntryId to object, increment nextEntryId, reset src attribute, and reset form inputs. #1

--- js/main.js
+++ js/main.js
@@ -7,3 +7,28 @@ function newPhoto(event) {
   var newImg = document.querySelector('#photo-url').value;
   $img.setAttribute('src', newImg);
 }
+
+var $codeJournal = document.querySelector('#code-journal');
+var defImage = 'images/placeholder-image-square.jpg';
+
+$codeJournal.addEventListener('submit', logValues);
+
+var valueObject = {};
+function logValues(event) {
+
+  event.preventDefault();
+  valueObject.title = $codeJournal.elements.title.value;
+  valueObject.photo_url = $codeJournal.elements.photo_url.value;
+  valueObject.notes = $codeJournal.elements.notes.value;
+  valueObject.nextEntryId = data.nextEntryId;
+  data.nextEntryId++;
+  $img.setAttribute('src', defImage);
+  $codeJournal.reset();
+}
+
+window.addEventListener('beforeunload', beforeUnload);
+
+function beforeUnload(event) {
+  var stringify = JSON.stringify(valueObject);
+  localStorage.setItem('storage', stringify);
+}
