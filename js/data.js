c15a0804da49af4fc1743ccaeafcb317e51b29eb
Alex Gonzalez <itsgonzalezalex@gmail.com>
2022-11-01 18:36:29 +0000

    Verify that new entries are saved to local storage along with the rest of the data model on page reload and Verify that each new entry receives an incrementing entryId. #1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,16 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var localPhotos = localStorage.getItem('photo-storage');
+
+if (localPhotos !== null) {
+  data = JSON.parse(localPhotos);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
+
+function beforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('photo-storage', dataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -13,22 +13,16 @@ var defImage = 'images/placeholder-image-square.jpg';
 
 $codeJournal.addEventListener('submit', logValues);
 
-var valueObject = {};
 function logValues(event) {
+  var valueObject = {};
 
   event.preventDefault();
   valueObject.title = $codeJournal.elements.title.value;
   valueObject.photo_url = $codeJournal.elements.photo_url.value;
   valueObject.notes = $codeJournal.elements.notes.value;
-  valueObject.nextEntryId = data.nextEntryId;
+  valueObject.entryId = data.nextEntryId;
   data.nextEntryId++;
+  data.entries.unshift(valueObject);
   $img.setAttribute('src', defImage);
   $codeJournal.reset();
-}
-
-window.addEventListener('beforeunload', beforeUnload);
-
-function beforeUnload(event) {
-  var stringify = JSON.stringify(valueObject);
-  localStorage.setItem('storage', stringify);
 }
