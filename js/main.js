a6bdd4c51e04d8c9447ba63627e29d123f5da5fe
Alex Gonzalez <itsgonzalezalex@gmail.com>
2022-11-03 05:51:45 +0000

    Ensure that each rendered entry is given a data-entry-id attribute indicating which entry it is, Listen for clicks on the parent element of all rendered entries, and show the entry form if an edit icon was clicked. #3

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@ function logValues(event) {
 function renderEntry(valueObject) {
   var createDivRow = document.createElement('div');
   createDivRow.setAttribute('class', 'row');
+  createDivRow.setAttribute('data-entry-id', valueObject.entryId);
   var createDivHalf = document.createElement('div');
   createDivHalf.setAttribute('class', 'column-half');
   createDivRow.appendChild(createDivHalf);
@@ -42,7 +43,7 @@ function renderEntry(valueObject) {
   createImg.setAttribute('src', valueObject.photo_url);
   createDivHalf.appendChild(createImg);
   var newDivHalf = document.createElement('div');
-  newDivHalf.setAttribute('class', 'column-half');
+  newDivHalf.setAttribute('class', 'column-half pen');
   createDivRow.appendChild(newDivHalf);
   var createUL = document.createElement('ul');
   newDivHalf.appendChild(createUL);
@@ -51,6 +52,10 @@ function renderEntry(valueObject) {
   var createH3 = document.createElement('h3');
   createH3.textContent = valueObject.title;
   createLi.appendChild(createH3);
+  var createPen = document.createElement('i');
+  createPen.setAttribute('class', 'fa-solid fa-pen');
+  createPen.setAttribute('data-view', 'edit-entry');
+  createH3.appendChild(createPen);
   var createP = document.createElement('p');
   createP.textContent = valueObject.notes;
   createLi.appendChild(createP);
@@ -59,6 +64,7 @@ function renderEntry(valueObject) {
 
 var $domContainer = document.querySelector('#dom');
 var $entryForm = document.querySelector('#entry-form');
+var $editEntry = document.querySelector('#edit-entry');
 
 document.addEventListener('DOMContentLoaded', function (e) {
   for (var i = 0; i < data.entries.length; i++) {
@@ -68,9 +74,15 @@ document.addEventListener('DOMContentLoaded', function (e) {
   if (data.view === 'entry-form') {
     $entryForm.className = 'view container';
     $domContainer.className = 'view container hidden';
+    $editEntry.className = 'view container hidden';
   } else if (data.view === 'entries') {
     $entryForm.className = 'view container hidden';
     $domContainer.className = 'view container';
+    $editEntry.className = 'view container hidden';
+  } else if (data.view === 'edit-entry') {
+    $entryForm.className = 'view container hidden';
+    $domContainer.className = 'view container hidden';
+    $editEntry.className = 'view container';
   }
 });
 
@@ -79,11 +91,11 @@ var $newA = document.querySelector('.new');
 
 $tabContainer.addEventListener('click', switchView);
 $newA.addEventListener('click', switchView);
+$domContainer.addEventListener('click', switchView);
 
 function switchView(event) {
   var dataView = event.target.getAttribute('data-view');
   var $viewDiv = document.querySelectorAll('.view');
-
   for (var j = 0; j < $viewDiv.length; j++) {
     if ($viewDiv[j].getAttribute('data-view') === dataView) {
       $viewDiv[j].className = 'view container';
